88eeba6b699c9ec6236c970ab5114d424883018b
Bruno Schaatsbergen <git@bschaatsbergen.com>
2021-11-15 16:38:16 +0100

    create some local listener utils and wip on trace

---
+++ pkg/utils/utils.go
@@ -0,0 +1,19 @@
+package utils
+
+import "strings"
+
+func IsIPv4(address string) bool {
+	return strings.Count(address, ":") < 2
+}
+
+func IsIPv6(address string) bool {
+	return strings.Count(address, ":") >= 2
+}
+
+func ReturnIPv4LocalListener() string {
+	return "0.0.0.0"
+}
+
+func ReturnIPv6LocalListener() string {
+	return "::"
+}
